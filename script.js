51446245e484a88ab6f0275f0948be7bd01d3572
Orhirsh <113570276+Orhirsh@users.noreply.github.com>
2023-09-06 22:17:28 -0700

    Added javascript and folded menu

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const foldMenu = document.querySelector(".fold-menu");
-    const hemMenu = document.querySelector(".hem-menu");
+const hemBtn = document.querySelector('.hem-menu');
+const foldedMenu = document.querySelector('.folded-menu');
 
-    hemMenu.addEventListener("click", function () {
-        // Toggle the "expanded" class to show/hide the menu
-        foldMenu.classList.toggle("expanded");
-    });
+let isMenuVisible = false; // Variable to track menu visibility
+
+hemBtn.addEventListener('click', function () {
+    // Toggle the display and play the animation
+    if (!isMenuVisible) {
+        foldedMenu.style.display = 'block';
+        foldedMenu.style.animationDirection = 'normal'; // Play the animation forward (fade in)
+        isMenuVisible = true; // Update menu visibility state
+    } else {
+        foldedMenu.style.animationDirection = 'reverse';
+        foldedMenu.style.animationFillMode = 'forwards'; // Keep the final state after animation
+        isMenuVisible = false; // Update menu visibility state
+    }
 });
